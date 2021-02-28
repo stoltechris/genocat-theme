63428c02163e424f0264bee7b1ed2622319372b7
Christian Stolte <christian.stolte@gmail.com>
2021-02-28 18:31:13 -0500

    WIP - not there yet.

---
+++ scripts/search_bar.js
@@ -0,0 +1,14 @@
+function search() {
+  var input, filter, x, a;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  x = document.getElementsByClassName("filterDiv");
+  for (i = 0; i < x.length; i++) {
+    a = x[i].getElementsByTagName("h4")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      x[i].style.display = "";
+    } else {
+      x[i].style.display = "none";
+    }
+  }
+}
